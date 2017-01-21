3d063a2a1c0885f4b991ef830ce80abb4ce4df9c
tmcgillicuddy <tmcgillicuddy12@gmail.com>
2017-01-21 14:53:04 -0500

    New Spawn System

--- WaveGame/Assets/Nils Code/enemyManager.cs
+++ WaveGame/Assets/Nils Code/enemyManager.cs
@@ -5,36 +5,53 @@ using UnityEngine;
 public class enemyManager : MonoBehaviour {
 
 	public GameObject enemy;
-	private int totalSpawns = 20;
+    private int totalSpawn, spawned;
 	private float frequency = 1;
-	public bool playing = true;
-	private int wave = 1;
-
-	public Transform[] spawnPoints;
+    public bool downTime, spawning;
+	private int wave = 0;
+    public List<Transform> spawnPoints;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (spawn (wave));
+		
 	}
-	
-	// Update is called once per frame
-	IEnumerator spawn(int wave) {
 
-		switch(wave)
-		{
-		case 1:
-			for (int i = 0; i < totalSpawns; i++) {
-				int location = Random.Range (0, 5);
-				Instantiate (enemy, spawnPoints[location].position, Quaternion.identity);				
-				yield return new WaitForSeconds (frequency);
-			}
-			yield return "done";
-			break;
-		default:
-			Instantiate (enemy, new Vector3 (0, 1, 48), Quaternion.identity);	
-			yield return "done";
-			break;
-		}
 
-	}
+    private void Update()
+    {
+        CountDown();
+        SpawnWave();
+    }
+
+    float timer;
+    void CountDown()
+    {
+        if(downTime == true)
+        {
+            timer -= Time.deltaTime;
+
+            if(timer <=0)
+            {
+                spawning = true;
+                downTime = false; 
+            }
+        }
+
+    }
+
+
+    void SpawnWave()
+    {
+        if (spawning == true)
+        {
+            if(spawned < totalSpawn)
+            {
+
+            }
+            else
+            {
+                spawning = false;
+            }
+        }
+    }
 }
