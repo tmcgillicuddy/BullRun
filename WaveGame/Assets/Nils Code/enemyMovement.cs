a51cb1baa7d24ab76dc2365e42daa4466268820c
Nils Steinbuegl <nils1723@gmail.com>
2017-01-20 20:13:33 -0500

    Please work

---
+++ WaveGame/Assets/Nils Code/enemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Time.deltaTime * speed;
+		transform.Translate (0, translation, 0);
+	}
+}
