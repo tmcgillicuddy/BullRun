6e1ff267a65e9b681db9ad8449acd655ed87421a
Nils Steinbuegl <nils1723@gmail.com>
2017-01-21 18:43:58 -0500

    Revert "Attack working better"
    This reverts commit b28fe347d1bd35536bd68304bae902e690832f2a.

--- WaveGame/Assets/Thomas Code/Crabhealth.cs
+++ WaveGame/Assets/Thomas Code/Crabhealth.cs
@@ -34,7 +34,7 @@ public class Crabhealth : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         print("Hit something");
-        if(collision.gameObject.tag == "AttackMop")
+        if(collision.gameObject.tag == "Mop")
         {
             print("Hit Mop");
             takeDamage(10);
