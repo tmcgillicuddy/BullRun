7f9f7375b7ba15dd4a5a48357d513059e8fc7e6b
Nils Steinbuegl <nils1723@gmail.com>
2017-01-22 00:25:40 -0500

    sinks

---
+++ WaveGame/Assets/Nils Code/sinkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinkManager : MonoBehaviour {
+
+	public sinkCluster cluster1, cluster2, cluster3, cluster4;
+	public int lives;
+
+	// Use this for initialization
+	void Start () {
+		updateHealth ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateHealth ();
+	}
+
+	void updateHealth()
+	{
+		lives = cluster1.health + cluster2.health + cluster3.health + cluster4.health;
+	}
+}
